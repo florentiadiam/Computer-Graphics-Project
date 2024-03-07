6a1a694f851f5449cccacbdcffd8e304028ea870
florentiadiam <florentiad@gmail.com>
2024-03-07 17:00:55 +0000

    prism added

--- tp3/MyPrism.js
+++ tp3/MyPrism.js
@@ -21,24 +21,24 @@ export class MyPrism extends CGFobject {
         var step = 2*Math.PI/this.slices;
 
         var height = 1.0; // Adjust the height of the prism as needed
-        var stacksheight= height/this.stacks;
+        var stacksheight= height/this.stacks; // 0.05
 
+    var startStack = height /  2
+    for(var j=0; j<this.stacks;j++){
         for (var i = 0; i < this.slices; i++) {
             // Vertices on the top face
-            this.vertices.push(Math.cos(ang), Math.sin(ang), height / 2);
+            this.vertices.push(Math.cos(ang) *0.5 , Math.sin(ang) *0.5, startStack);
             
             // Vertices on the bottom face
-            this.vertices.push(Math.cos(ang), Math.sin(ang), -height / 2);
+            this.vertices.push(Math.cos(ang) *0.5, Math.sin(ang)*0.5, -stacksheight +startStack);
         
             // Indices for the side faces
-            for(var j=0; j<1; j++){
-                this.indices.push(2 * i, (2 * i + 1) % (2 * this.slices), (2 * i + 2) % (2 * this.slices));
-                this.indices.push((2 * i + 1) % (2 * this.slices), (2 * i + 3) % (2 * this.slices), (2 * i + 2) % (2 * this.slices));    
-            }
+            this.indices.push(2 * i, (2 * i + 1) % (2 * this.slices), (2 * i + 2) % (2 * this.slices));
+            this.indices.push((2 * i + 1) % (2 * this.slices), (2 * i + 3) % (2 * this.slices), (2 * i + 2) % (2 * this.slices));    
               
             // Normals for the side faces (assuming a regular prism)
-            var normalX1 = Math.cos(Math.PI/2-ang);
-            var normalY1 = 0
+            var normalX1 = Math.cos(ang+ step/2);
+            var normalY1 = Math.sin(ang+ step/2);
             var normalZ = 0;
 
             // Top face normal
@@ -48,9 +48,37 @@ export class MyPrism extends CGFobject {
 
       
             ang += step;
-            this.primitiveType = this.scene.gl.TRIANGLES;
-            this.initGLBuffers();
         }
+        for (var i = 0; i < this.slices; i++) {
+            // Vertices on the top face
+            this.vertices.push(Math.cos(ang)*0.5, Math.sin(ang)*0.5,  startStack);
+            
+            // Vertices on the bottom face
+            this.vertices.push(Math.cos(ang)*0.5, Math.sin(ang)*0.5,  -stacksheight + startStack);
+        
+            // Indices for the side faces
+            this.indices.push(2 * i, (2 * i + 1) % (2 * this.slices), (2 * i + 2) % (2 * this.slices));
+            this.indices.push((2 * i + 1) % (2 * this.slices), (2 * i + 3) % (2 * this.slices), (2 * i + 2) % (2 * this.slices));    
+              
+            // Normals for the side faces (assuming a regular prism)
+            var normalX1 = Math.cos(ang- step/2);
+            var normalY1 = Math.sin(ang- step/2);
+            var normalZ = 0;
+
+            // Top face normal
+            this.normals.push(normalX1, normalY1, normalZ);
+            // Bottom face normal
+            this.normals.push(normalX1, normalY1, normalZ);
+
+      
+            ang += step;
+        
+        }
+        startStack += stacksheight
+    }
+        
+        this.primitiveType = this.scene.gl.TRIANGLES;
+        this.initGLBuffers();
     }
          /**
      * Called when user interacts with GUI to change object's complexity.
