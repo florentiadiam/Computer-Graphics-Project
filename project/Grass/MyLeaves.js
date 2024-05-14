3e9e6f5bff774d17266a1f9d2a94dfce7ba610e0
florentiadiam <florentiad@gmail.com>
2024-05-14 18:11:17 +0100

    new file created

---
+++ project/Grass/MyLeaves.js
@@ -0,0 +1,4 @@
+// Class to create the leaves of the grass
+//1. take one triangle and put it on the plane
+//2. change the features , and how to chang them
+//3. tha ones that should be random make them parameters
