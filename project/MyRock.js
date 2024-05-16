eb125579b5e87f6155e4f2e592a8750bb805fa4e
Maria Mitkou <up202311354@edu.pt>
2024-05-16 22:26:19 +0300

    rocks done

--- project/MyRock.js
+++ project/MyRock.js
@@ -1,4 +1,4 @@
-import { CGFobject } from '../lib/CGF.js';
+import { CGFobject } from '../../lib/CGF.js';
 
 export class MyRock extends CGFobject {
     constructor(scene, slices, stacks, inside) {
@@ -18,27 +18,35 @@ export class MyRock extends CGFobject {
         var ang = Math.PI / this.stacks;
         var step = 2 * Math.PI / this.slices;
 
-        for (var j = -Math.PI / 2; j <= Math.PI; j += ang) {
-            for (var i = 0; i <= this.slices; i++) {
+        for (var j = 0 ; j <=this.stacks; j ++) {
+            var a=j*ang;
+            var x1=Math.sin(a);
+            var y1=Math.cos(a)*0.5;
+            for (var i = 0; i <=this.slices; i++) {
+                var f=i*step;
+                var x2=Math.sin(f);
+                var z1=Math.cos(f);
+    
                 // Vertices
-                var x = Math.cos(i * step) * Math.cos(j);
-                var y = Math.sin(j) * 0.5; // Squish down the y-coordinate
-                var z = Math.sin(i * step) * Math.cos(j);
+                var X1 = z1*x1
+                var Y1 = y1
+                var Z1 = x2*x1
 
-                // Perturb the vertices along their normals to create slight irregularities
-                var perturbation = 0.2; // Adjust the perturbation factor as needed
-                var normalFactor = 1 + (Math.random() - 0.5) * perturbation; // Random factor to perturb along normal
-                x *= normalFactor;
-                y *= normalFactor;
-                z *= normalFactor;
-
-                this.vertices.push(x, y, z);
+                       // Perturb the vertices along their normals to create slight irregularities
+              var perturbation = 0.2; // Adjust the perturbation factor as needed
+              var normalFactor = 1 + (Math.random() - 0.5) * perturbation; // Random factor to perturb along normal
+              
+                X1 *= normalFactor;
+                Y1 *= normalFactor;
+                Z1 *= normalFactor;
+                
+                this.vertices.push(X1, Y1, Z1)
 
                 if (this.inside == false) {
                     // Normal
-                    this.normals.push(-x, -y, -z);
+                    this.normals.push(-X1, -Y1, -Z1);
                 } else {
-                    this.normals.push(x, y, z);
+                    this.normals.push(X1, Y1, Z1);
                 }
 
                 // Texture coordinates
@@ -46,6 +54,9 @@ export class MyRock extends CGFobject {
                 var t = (j + Math.PI / 2) / Math.PI; // Vertical (t) coordinate
                 this.texCoords.push(-s, -t);
             }
+       
+
+          
         }
 
         var count = 0;
--- project/MyRockSet.js
+++ project/MyRockSet.js
@@ -1,5 +1,5 @@
-import { CGFappearance, CGFobject } from '../lib/CGF.js';
-import { MyPlane } from './MyPlane.js';
+import { CGFappearance, CGFobject } from '../../lib/CGF.js';
+import { MyPlane } from '../MyPlane.js';
 import { MyRock } from "./MyRock.js";
 
 export class MyRockSet extends CGFobject {
@@ -11,12 +11,14 @@ export class MyRockSet extends CGFobject {
         this.RockScale = [];
         this.RockPos_x = [];
         this.RockPos_y = [];
-        this.numRocks = Math.random() * (20 - 5) + 5;
+        this.rock =  new MyRock(this.scene, 10, 10, false);
+        this.numRocks = 300;
+ 
         for (var i = 0; i <= this.numRocks; i++) {
             this.RockScale.push(Math.random() * (1 - 0.5) + 0.5);
             this.RockAngle.push(Math.random() * (Math.PI / 4 + Math.PI / 4)-Math.PI / 4);
-            this.RockPos_x.push(Math.random()*(15+15)-15)
-            this.RockPos_y.push(Math.random()*(15+15)-15)
+            this.RockPos_x.push(Math.random()*(5+5)-5)
+            this.RockPos_y.push(Math.random()*(5+5)-5)
         }
         this.plane = new MyPlane(this.scene,30);
         this.initMaterials();
@@ -27,34 +29,39 @@ export class MyRockSet extends CGFobject {
         this.rockMaterial.setDiffuse(0.9, 0.9, 0.9, 1);
         this.rockMaterial.setSpecular(0.1, 0.1, 0.1, 1);
         this.rockMaterial.setShininess(10.0);
-        this.rockMaterial.loadTexture("images/rock.jpg");
+        this.rockMaterial.loadTexture("images/rocks.jpg");
         this.rockMaterial.setTextureWrap('REPEAT', 'REPEAT');
-
-        this.terainMaterial = new CGFappearance(this.scene);
-        this.terainMaterial.setAmbient(0.1, 0.1, 0.1, 1);
-        this.terainMaterial.setDiffuse(0.9, 0.9, 0.9, 1);
-        this.terainMaterial.setSpecular(0.1, 0.1, 0.1, 1);
-        this.terainMaterial.setShininess(10.0);
-        this.terainMaterial.loadTexture("images/grass.jpg");
-        this.terainMaterial.setTextureWrap('REPEAT', 'REPEAT');
 
     }
 
     display() {
-        this.scene.pushMatrix();
-        this.terainMaterial.apply();
-        this.scene.scale(30, 30, 30);
-        this.scene.rotate(-Math.PI / 2.0, 1, 0, 0);
-        this.plane.display();
-        this.scene.popMatrix();
 
-        for (let i = 0; i < this.numRocks; i++) {
+        for (let i = 0; i < 100; i++) {
             this.scene.pushMatrix();
             this.rockMaterial.apply();
             this.scene.translate(this.RockPos_x[i],0.4,this.RockPos_y[i]);
             this.scene.scale(this.RockScale[i], this.RockScale[i], this.RockScale[i]);
             this.scene.rotate(this.RockAngle[i], 0, 0, 1);
-            this.rock = new MyRock(this.scene, 10, 10, false);
+            this.rock.display();
+            this.scene.popMatrix();
+        }
+
+        for (let j=100; j< 200; j++){
+            this.scene.pushMatrix();
+            this.rockMaterial.apply();
+            this.scene.translate(this.RockPos_x[j]/2,1.4,this.RockPos_y[j]/2);
+            this.scene.scale(this.RockScale[j], this.RockScale[j], this.RockScale[j]);
+            this.scene.rotate(this.RockAngle[j], 0, 0, 1);
+            this.rock.display();
+            this.scene.popMatrix();
+        }
+
+        for (let z=200; z< 300; z++){
+            this.scene.pushMatrix();
+            this.rockMaterial.apply();
+            this.scene.translate(this.RockPos_x[z]/4,2.4,this.RockPos_y[z]/4);
+            this.scene.scale(this.RockScale[z], this.RockScale[z], this.RockScale[z]);
+            this.scene.rotate(this.RockAngle[z], 0, 0, 1);
             this.rock.display();
             this.scene.popMatrix();
         }
